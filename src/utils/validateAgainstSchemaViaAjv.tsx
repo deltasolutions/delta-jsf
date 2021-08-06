95d09d7512334f49bdc1ab57056a95eed19d29ef
albnnc <albnnc@gmail.com>
2021-08-06 13:33:51 +0300

    Make default validation safer in terms of exception handling

--- src/utils/validateAgainstSchemaViaAjv.tsx
+++ src/utils/validateAgainstSchemaViaAjv.tsx
@@ -15,24 +15,29 @@ export const validateAgainstSchemaViaAjv = ({
   value,
   transformAjvErrors
 }: ValidateAgainstSchemaViaAjvOptions): Validity => {
-  const validateViaAjv = ajv.compile(schema);
-  validateViaAjv(value);
-  const ajvErrors =
-    transformAjvErrors?.(validateViaAjv.errors ?? []) ??
-    validateViaAjv.errors ??
-    [];
-  const validity =
-    ajvErrors.reduce((prev, curr) => {
-      const path = curr.instancePath
-        .replace(/\/(\D)/g, '/properties/$1')
-        .replace(/\/(\d)/g, '/items/$1')
-        .split('/')
-        .map(v => v.replace(/~0/g, '~').replace(/~1/g, '/'))
-        .slice(1);
-      path.push('errors');
-      const existingValue = get(prev, path) ?? [];
-      set(prev, path, [...existingValue, curr.message]);
-      return prev;
-    }, {}) ?? {};
-  return validity;
+  try {
+    const validateViaAjv = ajv.compile(schema);
+    validateViaAjv(value);
+    const ajvErrors =
+      transformAjvErrors?.(validateViaAjv.errors ?? []) ??
+      validateViaAjv.errors ??
+      [];
+    const validity =
+      ajvErrors.reduce((prev, curr) => {
+        const path = curr.instancePath
+          .replace(/\/(\D)/g, '/properties/$1')
+          .replace(/\/(\d)/g, '/items/$1')
+          .split('/')
+          .map(v => v.replace(/~0/g, '~').replace(/~1/g, '/'))
+          .slice(1);
+        path.push('errors');
+        const existingValue = get(prev, path) ?? [];
+        set(prev, path, [...existingValue, curr.message]);
+        return prev;
+      }, {}) ?? {};
+    return validity;
+  } catch (e) {
+    console.warn(e);
+    return {};
+  }
 };
